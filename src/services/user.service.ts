faa2c58c5cc7e90d731c47901b435428c32677f3
ShadowAdi <shadowshukla76@gmail.com>
2026-01-21 17:30:41 +0530

    added user class service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,37 @@
-class UserService{
-    async createUser(){
-        
+import { logger } from "../config/logger";
+import { User } from "../models/user.model";
+import { CreateUserDto } from "../types/user/create-user.dto";
+import { AppError } from "../utils/AppError";
+import { hashPassword } from "../utils/password-utils";
+
+class UserClassService {
+    async createUser(payload: CreateUserDto) {
+        try {
+            const exists = await User.findOne({
+                email: payload.email
+            })
+            if (exists) {
+                logger.error(`User Already Exists`)
+                throw new AppError(`User Already Exists`, 409)
+            }
+            const hashedPassword = await hashPassword(payload.password)
+
+            const user = await User.create({
+                ...payload,
+                password: hashedPassword
+            })
+            return {
+                username: user.username,
+                email: user.email,
+                _id: user._id,
+                createdAt: user.createdAt
+            }
+        } catch (error) {
+            logger.error(`Failed to create user: ${error}`)
+            console.error(`Failed to create user: ${error}`)
+            throw new AppError(`Failed to create user`, 500)
+        }
     }
 }
+
+export const UserService = new UserClassService()
