6aaa4d6fc862a7a3427729025ce0244b266a8322
ShadowAdi <shadowshukla76@gmail.com>
2026-01-21 21:00:42 +0530

    added validators

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,19 +1,42 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller";
+import { createUserValidator, updateUserValidator, userIdValidator } from "../validators/user.validator";
+import { validate } from "../middlewares/validation.middleware";
 
 export const userRouter = Router();
 
 // Create a new user
-userRouter.post("/", UserController.createUser.bind(UserController));
+userRouter.post(
+    "/", 
+    createUserValidator, 
+    validate, 
+    UserController.createUser.bind(UserController)
+);
 
 // Get all users
 userRouter.get("/", UserController.getAllUsers.bind(UserController));
 
 // Get user by ID
-userRouter.get("/:userId", UserController.getUserById.bind(UserController));
+userRouter.get(
+    "/:userId", 
+    userIdValidator, 
+    validate, 
+    UserController.getUserById.bind(UserController)
+);
 
 // Update user by ID
-userRouter.patch("/:userId", UserController.updateUser.bind(UserController));
+userRouter.patch(
+    "/:userId", 
+    userIdValidator, 
+    updateUserValidator, 
+    validate, 
+    UserController.updateUser.bind(UserController)
+);
 
 // Delete user by ID
-userRouter.delete("/:userId", UserController.deleteUser.bind(UserController));
+userRouter.delete(
+    "/:userId", 
+    userIdValidator, 
+    validate, 
+    UserController.deleteUser.bind(UserController)
+);
