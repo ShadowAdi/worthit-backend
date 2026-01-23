d9079d1c867f3a52c7d9eab166915e463c5d6b00
ShadowAdi <shadowshukla76@gmail.com>
2026-01-24 01:45:09 +0530

    added create review

---
+++ src/services/review.service.ts
@@ -0,0 +1,5 @@
+class ReviewService{
+    async createReview(){
+        
+    }
+}
---
+++ src/types/review/review-create.dto.ts
@@ -0,0 +1,9 @@
+
+export interface CreateReviewDto {
+    usageStatus: "used" | "gifted" | "not_used";
+    worthTheMoney: boolean;
+    biggestDisappointment: string;
+    whoShouldNotBuy: string;
+    recommendation: "recommend" | "not_recommend";
+    additionalNotes?: string;
+}
